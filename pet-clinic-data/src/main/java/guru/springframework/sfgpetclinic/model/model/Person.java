3f8fe5b9e08248623bf87d6be03ef5c86a0fa74e
hasika-sandaruwan <hasikasandaruwan.info@gmail.com>
2021-08-17 18:27:49 +0530

    create all #1

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/model/Person.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/model/Person.java
@@ -1,6 +1,6 @@
-package guru.springframework.sfgpetclinic.model;
+package guru.springframework.sfgpetclinic.model.model;
 
-public class Person {
+public class Person extends BaseEntity{
     private String firstName;
     private String lastName;
 
--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/model/PetType.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/model/PetType.java
@@ -1,6 +1,6 @@
-package guru.springframework.sfgpetclinic.model;
+package guru.springframework.sfgpetclinic.model.model;
 
-public class PetType {
+public class PetType extends BaseEntity{
     private String name;
 
     public String getName() {
